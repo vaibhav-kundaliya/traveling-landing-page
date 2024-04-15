cfe50385409bc47f60f45a22ef7ef25e76e57f38
JARVIS1612 <81670956+JARVIS1612@users.noreply.github.com>
2024-04-16 02:17:53 +0530

    normal header completed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
+import Header  from "./components/header";
+
 export default function Home() {
     return (
         <>
-            <h1>Header</h1>
-            <h1>Hero Section</h1>
-            <h1>Brands logos</h1>
-            <h1>Services</h1>
-            <h1>Travel point</h1>
-            <h1>Newslatter subscription</h1>
-            <h1>Footer</h1>
+            <Header />
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            fontWeight: {
+                regular: 400,
+                medium: 500,
+                semibold: 600,
+                bold: 700,
+                extrabold: 800,
+                black: 900,
+                weight:1000
+            },
+            colors:{
+                lightpurple: "#5D50C6"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
